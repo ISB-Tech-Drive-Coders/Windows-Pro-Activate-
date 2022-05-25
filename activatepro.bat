29f7207ac792a927e86e8c80c7c9e03405fd7d7e
Ravin-Oli <77025041+Ravin-Oli@users.noreply.github.com>
2022-05-24 19:40:21 -0700

    Create activatepro.bat

---
+++ activatepro.bat
@@ -0,0 +1,3 @@
+echo "This will activate Windows 10 Pro on the PC"
+cscript slmgr.vbs /ipk "W269N-WFGWX-YVC9B-4J6C9-T83GX" & cscript slmgr.vbs /skms kms.ddns.net & cscript slmgr.vbs /ato
+echo "Finished with Windows 10 Pro Installation, if you got a error then please try again running this as a administrator."
